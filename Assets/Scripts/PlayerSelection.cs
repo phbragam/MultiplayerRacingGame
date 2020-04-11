b95c314fb90bbdcdf89412145a7a78c931981aaf
Pedro Braga <phbm.braga@gmail.com>
2020-04-11 19:34:16 -0300

    Racing game mode: player selection UI

---
+++ Assets/Scripts/PlayerSelection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelection : MonoBehaviour
+{
+    public GameObject[] SelectablePlayers;
+    public int PlayerSelectionNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerSelectionNumber = 0;
+
+        ActivatePlayer(PlayerSelectionNumber);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ActivatePlayer(int x)
+    {
+        foreach (GameObject SelectablePlayer in SelectablePlayers)
+        {
+            SelectablePlayer.SetActive(false);
+        }
+        SelectablePlayers[x].SetActive(true);
+    }
+
+    public void NextPlayer()
+    {
+        PlayerSelectionNumber += 1;
+        if(PlayerSelectionNumber >= SelectablePlayers.Length)
+        {
+            PlayerSelectionNumber = 0;
+        }
+
+        ActivatePlayer(PlayerSelectionNumber);
+    }
+
+    public void PreviousPlayer()
+    {
+        PlayerSelectionNumber -= 1;
+
+        if (PlayerSelectionNumber < 0)
+        {
+            PlayerSelectionNumber = SelectablePlayers.Length - 1;
+        }
+        ActivatePlayer(PlayerSelectionNumber);
+    }
+}
