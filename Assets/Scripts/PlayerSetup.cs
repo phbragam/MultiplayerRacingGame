190854e3fc6266faf393131de1dba50d1d4f4f7a
Pedro Braga <phbm.braga@gmail.com>
2020-04-13 10:09:06 -0300

    Rc: player setup script and synchronizing movement across the network

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSetup : MonoBehaviourPunCallbacks
+{
+    public Camera PlayerCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            // enable car movement script and camera
+            GetComponent<CarMovement>().enabled = true;
+            PlayerCamera.enabled = true;
+        }
+        else
+        {
+            // player is remote. Disable carMovement script and camera
+            GetComponent<CarMovement>().enabled = false;
+            PlayerCamera.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
