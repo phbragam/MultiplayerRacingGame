8c8dc69b62b8baa52822fadb20a8faacdb454467
Pedro Braga <phbm.braga@gmail.com>
2020-04-16 20:29:22 -0300

    Rc: shooting with laser gun

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -8,15 +8,28 @@ public class Shooting : MonoBehaviour
     public Transform firePosition;
     public Camera PlayerCamera;
 
+
+
     public DeathRacePlayer DeathRacePlayerProperties;
 
     private float fireRate;
     private float fireTimer = 0.0f;
+    private bool useLaser;
+    public LineRenderer lineRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         fireRate = DeathRacePlayerProperties.fireRate;
+
+        if (DeathRacePlayerProperties.weaponName == "Laser Gun")
+        {
+            useLaser = true;
+        }
+        else
+        {
+            useLaser = false;
+        }
     }
 
     // Update is called once per frame
@@ -41,8 +54,39 @@ public class Shooting : MonoBehaviour
 
     public void Fire()
     {
-        Ray ray = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-        GameObject bulletGameObject = Instantiate(BulletPrefab, firePosition.position, Quaternion.identity);
-        bulletGameObject.GetComponent<BulletScript>().Initialize(ray.direction, DeathRacePlayerProperties.bulletSpeed, DeathRacePlayerProperties.damage);
+        if (useLaser)
+        {
+            // Laser codes
+            RaycastHit _hit;
+            Ray ray = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+            if (Physics.Raycast(ray, out _hit, 200))
+            {
+                if (!lineRenderer.enabled)
+                {
+                    lineRenderer.enabled = true;
+                }
+
+                lineRenderer.startWidth = 0.3f;
+                lineRenderer.endWidth = 0.1f;
+
+                lineRenderer.SetPosition(0, firePosition.position);
+                lineRenderer.SetPosition(1, _hit.point);
+
+                StopAllCoroutines();
+                StartCoroutine(DisableLaserAfterSecs(0.3f));
+            }
+        }
+        else
+        {
+            Ray ray = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+            GameObject bulletGameObject = Instantiate(BulletPrefab, firePosition.position, Quaternion.identity);
+            bulletGameObject.GetComponent<BulletScript>().Initialize(ray.direction, DeathRacePlayerProperties.bulletSpeed, DeathRacePlayerProperties.damage);
+        }
+    }
+
+    IEnumerator DisableLaserAfterSecs(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        lineRenderer.enabled = false;
     }
 }
